f632bd413e0d4a63a4430f5a0e73c415c1a6e1bd
Adrian Bielefeldt <Adrian.Bielefeldt@condat.de>
2024-03-05 10:50:13 +0100

    This works but feels VERY hacky.

--- app/provider/properties/parts/Util.js
+++ app/provider/properties/parts/Util.js
@@ -52,17 +52,14 @@ export const setExpression = (bpmnFactory, businessObject, field, value) => {
     }
 
     let extensionElements = getExtensionElements(bpmnFactory, businessObject);
-    if (!extensionElements) {
-        extensionElements = bpmnFactory.create("bpmn:ExtensionElements");
-    }
-    let elem = extensionElements.get("values").find((e) => {
+    let elem = extensionElements.values.find((e) => {
         return e.$type === "activiti:Field" && e.name === property;
     });
     if (!elem) {
         elem = bpmnFactory.create("activiti:Field", {
             name: property,
         });
-        extensionElements.get("values").push(elem);
+        extensionElements.values.push(elem);
     }
     setExpressionValue(elem, value, bpmnFactory)
     return extensionElements
@@ -70,28 +67,35 @@ export const setExpression = (bpmnFactory, businessObject, field, value) => {
 
 export const createListener = (businessObject, bpmnFactory) => {
     let extensionElements = getExtensionElements(bpmnFactory, businessObject);
-    if (!extensionElements) {
-        extensionElements = bpmnFactory.create("bpmn:ExtensionElements");
-    }
-    let listener = extensionElements.get("values").find((e) => {
+    let listener = extensionElements.values.find((e) => {
         return e.$type === "activiti:ExecutionListener";
     });
     if (!listener) {
         listener = bpmnFactory.create("activiti:ExecutionListener");
-        extensionElements.get("values").push(listener);
+        extensionElements.values.push(listener);
     }
     return {extensionElements, listener}
 }
 
 export const getExtensionElements = (bpmnFactory, businessObject) => {
     const extensionElements = businessObject.extensionElements;
-    const preexistingValues = extensionElements ? extensionElements.get('values') : [];
-    return bpmnFactory.create('bpmn:ExtensionElements', { values: preexistingValues.slice() });
+    let preexistingValues = extensionElements ? extensionElements.values : [];
+    preexistingValues = preexistingValues.map(preValue => bpmnFactory.create(
+      preValue.$type,
+      {
+          "name": preValue.name,
+          "string": bpmnFactory.create(
+            preValue.string.$type,
+            {
+                "text": preValue.string.text
+            }
+          )
+      }
+    ))
+    return bpmnFactory.create('bpmn:ExtensionElements', { values: preexistingValues });
 }
 
 export const updateElement = (element, modeling, extensionElements) => {
     element = element.labelTarget || element;
-    console.log("Updating " + JSON.stringify(element))
-    console.log(JSON.stringify(extensionElements))
     return modeling.updateProperties(element, { extensionElements });
 }
